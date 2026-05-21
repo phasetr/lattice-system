bf8bdf66845f8921fc35712d823be650fc6303ab
phasetr <phasetr@gmail.com>
2026-05-22 08:40:07 +0900

    Refactor Tasaki local-difference raising API -- #412 (#3488)
    * Start Tasaki local-difference raising refactor
    
    * Split Tasaki local-difference raising API
    
    * Fix Tasaki local-difference module doc

--- LatticeSystem/Quantum/SpinS/Theorem23IntervalCasimir.lean
+++ LatticeSystem/Quantum/SpinS/Theorem23IntervalCasimir.lean
@@ -1,5 +1,6 @@
 import LatticeSystem.Quantum.SpinS.Theorem23Interval
 import LatticeSystem.Quantum.SpinS.Theorem23LocalCoefficient
+import LatticeSystem.Quantum.SpinS.Theorem23LocalDifferenceMarshall
 
 /-!
 # Tasaki §2.5 Theorem 2.3 interval Casimir API
