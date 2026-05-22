26d2f2973bd614b1be91e9792b6ab4064e995097
phasetr <phasetr@gmail.com>
2026-05-22 09:34:16 +0900

    Refactor Tasaki predicted ladder API -- #412 (#3490)
    * Start Tasaki predicted ladder refactor -- #412
    
    * Split Tasaki predicted ladder wrappers -- #412

--- LatticeSystem/Quantum/SpinS/Theorem23SectorExistence.lean
+++ LatticeSystem/Quantum/SpinS/Theorem23SectorExistence.lean
@@ -1,4 +1,5 @@
 import LatticeSystem.Quantum.SpinS.Theorem23Dominance
+import LatticeSystem.Quantum.SpinS.Theorem23PredictedLadder
 import LatticeSystem.Quantum.SpinS.Theorem23LocalCoefficient
 import LatticeSystem.Quantum.SpinS.Theorem23LocalDifferenceMarshall
 
